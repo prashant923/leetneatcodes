0326d5e3c19ebb65aafb2febcdbca2738183aa88
Prashant singh <112719877+prashant923@users.noreply.github.com>
2024-02-10 08:46:06 +0000

    new sol

---
+++ leetcode75/3028.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0;
+        int count =0;
+        for(int num : nums){
+             sum += num;
+             if(sum==0){
+                 count++;
+             }
+
+        }
+        return count;
+    }
+}
